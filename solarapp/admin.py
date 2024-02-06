2555bd0279ca69644ba5a85dc82dddfc197133da
ibkay998 <ibukun46@gmail.com>
2024-02-06 22:57:00 +0100

    latest changes

--- solarapp/admin.py
+++ solarapp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Installer, UserProfile
+from .models import Installer, UserProfile,CustomUser
 
-# Register your models here.
-admin.site.register(Installer)
-admin.site.register(UserProfile)
+from django.contrib import admin
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # Customize admin interface for installers
+    pass
+
+@admin.register(Installer)
+class InstallerAdmin(admin.ModelAdmin):
+    # Customize admin interface for installers
+    pass
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    # Customize admin interface for user profiles
+    pass
--- solarapp/models.py
+++ solarapp/models.py
@@ -1,35 +1,30 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
-from django.contrib.auth.hashers import make_password
-from django.utils.crypto import get_random_string
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from enum import Enum
 
-class Installer(AbstractUser):
-    # Installer Profile model
-    pass
+class CustomUser(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('user', 'User'),
+        ('installer', 'Installer'),
+    ]
+    
+    user_type = models.CharField(max_length=20, choices=USER_TYPE_CHOICES)
 
+    
+class Installer(CustomUser):
+    class Meta:
+        proxy = True
 
-class UserProfile(models.Model):
-    # User Profile model
-    username = models.CharField(max_length=100)
-    email = models.EmailField(default='jamesBrown@greek.com')
-    password = models.CharField(max_length=100,default=make_password(password="password123"))
-
-    first_name = models.CharField(max_length=30,default='James')
-    last_name = models.CharField(max_length=30,default='Brown')
-
-    installer = models.ForeignKey(Installer, on_delete=models.SET_DEFAULT, default=None, null=True, related_name='users')
-
+class UserProfile(CustomUser):
+    class Meta:
+        proxy = True
 
 # Creating an instance of installer_0
-installer_0, created = Installer.objects.get_or_create(username='installer_0')
+# installer_0, created = Installer.objects.get_or_create(username='installer_0')
 
-# Signal to handle deletion of installers
-@receiver(pre_delete, sender=Installer)
-def pre_delete_installer(sender, instance, **kwargs):
-    if instance != installer_0:
-        # Assign users within the deleted installer to installer_0
-        UserProfile.objects.filter(installer=instance).update(installer=installer_0)
+# # Signal to handle deletion of installers
+# @receiver(pre_delete, sender=Installer)
+# def pre_delete_installer(sender, instance, **kwargs):
+#     if instance != installer_0:
+#         # Assign users within the deleted installer to installer_0
+#         UserProfile.objects.filter(installer=instance).update(installer=installer_0)
